180c23ca895bd378292853b91c5c1ca3126bbd60
To Ngoc Tien <24280109@student.hcmus.edu.vn>
2026-06-09 17:37:07 +0700

    feat: update cpp/128-longest-consecutive-sequence

--- cpp/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ cpp/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -3,13 +3,15 @@ public:
     int longestConsecutive(vector<int>& nums) {
         unordered_set<int> s(nums.begin(), nums.end());
         int maxLen = 0;
-        for(const auto& num : s){
-            if(s.find(num - 1) == s.end()){
-                int next = num + 1;
-                while(s.find(next) != s.end()){
-                    next++;
+        for (const auto& num : s){
+            if (s.find(num - 1) == s.end()) {
+                int length = 1;
+                int current = num;
+                while(s.find(current + 1) != s.end()) {
+                    current++;
+                    length++;
                 }
-                maxLen = max(next - num, maxLen);
+                maxLen = max(maxLen, length);
             }
         }
         return maxLen;
