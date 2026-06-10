f49b52b322c88303848fe3c025d567f276a9523c
Kien Pham <108476929+kienpham07@users.noreply.github.com>
2026-06-10 12:10:10 +0700

    feat: add java/141-linked-list-cycle (#78)

---
+++ java/141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+
+        while (fastPointer.next != null && fastPointer.next.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+
+            if (slowPointer == fastPointer) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    // TC: O(n), SC: O(1);
+}
