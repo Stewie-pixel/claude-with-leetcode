505dddd3c3b23cc4092464b93e694877362c0bd8
Stewie-pixel <hanchivi39@gmail.com>
2026-07-22 09:28:49 +0000

    Update README table

---
+++ cpp/860-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five, ten = 0;
+
+        for (int b : bills) {
+            if (b == 5) {
+                five++;
+            }
+            else if (b == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            }
+            else {
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
